2730d306cd7ad46c10e46676ed8b31dd0b951bf0
TMBilalTM <aksbilalarda@gmail.com>
2024-09-18 14:28:19 +0300

    BilalTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "FileBox",
   description: "FileBox is a seamless and secure file-sharing platform that allows users to upload, store, and share files effortlessly. Whether you're collaborating with colleagues or sharing personal files with friends, FileBox provides a fast, reliable, and intuitive solution to keep your data safe and accessible from anywhere. With robust privacy controls and easy-to-use features, managing your files has never been easier.",
+  icons: {
+    icon: "/favicon.ico", // Favicon dosyanızın yolu
+  },
 };
 
 export default function RootLayout({
